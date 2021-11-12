1af4d76a72dae2a94f9c5f034dcea102d91100a4
Antoine Rawlins <twanrawlins@outlook.com>
2021-11-12 13:55:48 -0600

    end of section 10

--- API/Startup.cs
+++ API/Startup.cs
@@ -18,6 +18,8 @@ using Persistence;
 using AutoMapper;
 using Application.Core;
 using API.Extensions;
+using FluentValidation.AspNetCore;
+using API.Middleware;
 
 namespace API
 {
@@ -34,7 +36,10 @@ namespace API
     public void ConfigureServices(IServiceCollection services)
     {
 
-      services.AddControllers();
+      services.AddControllers().AddFluentValidation(config =>
+      {
+        config.RegisterValidatorsFromAssemblyContaining<Create>();
+      });
       services.AddApplicationServices(_config);
      
     }
@@ -42,9 +47,11 @@ namespace API
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
+      app.UseMiddleware<ExceptionMiddleware>();
+
+
       if (env.IsDevelopment())
       {
-        app.UseDeveloperExceptionPage();
         app.UseSwagger();
         app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
       }
---
+++ client-app/src/stores/commonStore.ts
@@ -0,0 +1,15 @@
+import { makeAutoObservable } from "mobx";
+import { ServerError } from "../app/models/ServerError";
+
+export default class CommonStore {
+  error: ServerError | null = null;
+
+  constructor() {
+    makeAutoObservable(this)
+  }
+
+  serServerError = (error: ServerError) => {
+    this.error = error
+  }
+
+}

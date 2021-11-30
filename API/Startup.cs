b232b989264feaecb16f69ef8447daf1bdb74c30
Antoine Rawlins <twanrawlins@outlook.com>
2021-11-30 16:05:48 -0600

    end of section 12

--- API/Startup.cs
+++ API/Startup.cs
@@ -20,6 +20,8 @@ using Application.Core;
 using API.Extensions;
 using FluentValidation.AspNetCore;
 using API.Middleware;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
 
 namespace API
 {
@@ -36,11 +38,16 @@ namespace API
     public void ConfigureServices(IServiceCollection services)
     {
 
-      services.AddControllers().AddFluentValidation(config =>
+      services.AddControllers(opt => {
+        var policy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+        opt.Filters.Add(new AuthorizeFilter(policy));
+      })
+        .AddFluentValidation(config =>
       {
         config.RegisterValidatorsFromAssemblyContaining<Create>();
       });
       services.AddApplicationServices(_config);
+      services.AddIdentityServices(_config);
      
     }
 
@@ -62,6 +69,7 @@ namespace API
 
       app.UseCors("CorsPolicy");
 
+      app.UseAuthentication();
       app.UseAuthorization();
 
       app.UseEndpoints(endpoints =>
